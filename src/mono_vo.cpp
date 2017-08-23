3e4fad7e65abff431328f187d8d60f770c3af1c2
Andrew Dahdouh <andrew.dahdouh@gmail.com>
2017-08-23 06:35:09 -0500

    Added rest of basic algorithm. Need to go back and heavily debug each portion.

--- src/mono_vo.cpp
+++ src/mono_vo.cpp
@@ -22,7 +22,7 @@
 
 
 
-#define MIN_NUM_FEAT 50 // TODO: make a yaml param
+#define MIN_NUM_FEAT 50 // TODO: make a yaml param? 
 
 cv::Mat init_1_c, init_2_c; //These are the two color images we will use// .
 cv::Mat init_1, init_2; //These are the BW images that will be converted from the color images.
@@ -33,7 +33,7 @@ cv::Mat E, R, t, mask, R_f, t_f; //variables for the function.
 std::vector<cv::Point2f> points1, points2, prevFeatures, currFeatures; //Vectors to store the coordinates of feature points.
 std::vector<uchar> status;
 double focal_length;
-double scale = 2.00;
+double scale = 2.00; //TODO Figure out how to add or impliment scaling fundtion.
 
 bool init= false;
 
@@ -42,19 +42,20 @@ void imageCallback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::C
 {
   try
   {
-      cv::imshow("view", cv_bridge::toCvShare(image, "bgr8")->image);
+    cv::imshow("view", cv_bridge::toCvShare(image, "bgr8")->image);
       // Init logic to ensure two images are captured for initial features
-      if (!init)
-      {
-          init_2_c = cv_bridge::toCvShare(image, "bgr8")->image;
-          if (!init_1_c.empty() && !init_2_c.empty()) {
-              std::cout << "Initializing..." << std::endl;
+    if (!init)
+    {
+     // TODO: Visualize Keypoints being tracked.   
+      init_2_c = cv_bridge::toCvShare(image, "bgr8")->image;
+      if (!init_1_c.empty() && !init_2_c.empty()) {
+        std::cout << "Initializing..." << std::endl;
 
               //Convert to grayscale.
-              cv::cvtColor(init_1_c, init_1, COLOR_BGR2GRAY);
-              cv::cvtColor(init_2_c, init_2, COLOR_BGR2GRAY);
+        cv::cvtColor(init_1_c, init_1, COLOR_BGR2GRAY);
+        cv::cvtColor(init_2_c, init_2, COLOR_BGR2GRAY);
 
-              //feature detection and tracking.
+            //feature detection and tracking.
               featureDetection(init_1, points1); //detect features in Img1
               featureTracking(init_1, init_2, points1, points2, status); //track those features to init_2
 
@@ -88,15 +89,66 @@ void imageCallback(const sensor_msgs::ImageConstPtr& image, const sensor_msgs::C
 
       if(init) //Main portion of callback right now.
       {
-          std::cout << "Visual Odom Publishing..." << std::endl;
-      }
-      cv::waitKey(30);
+        std::cout << "Visual Odom Publishing..." << std::endl;
+          // std::cout << "In main loop" << std::endl;
+          //   currImage_c = cv_bridge::toCvShare(image, "bgr8")->image;
+          //   cvtColor(currImage_c,currImage, COLOR_BGR2GRAY);
+          //   std::cout << "In after colorchange" << std::endl;
 
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", image->encoding.c_str());
-  }
+          //   featureTracking(prevImage,currImage, prevFeatures, currFeatures, status);
+          //   std::cout << "feat track" << std::endl;
+
+          //   cv::Point2d ppoint(cam_info->K[2], cam_info->K[5]); //principle point
+          //   std::cout << "principle point" << std::endl;
+
+          //   E = cv::findEssentialMat(prevFeatures, currFeatures, focal_length, ppoint, RANSAC, 0.999, 1.0, mask);
+          //   cv::recoverPose(E, prevFeatures, currFeatures, R, t, focal_length, ppoint, mask);
+
+          //   cv::Mat prevPts(2, prevFeatures.size(), CV_64F);
+          //   cv::Mat currPts(2, currFeatures.size(), CV_64F);
+
+          //   for(int i=0;i<prevFeatures.size();i++)  {   //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
+          //       prevPts.at<double>(0,i) = prevFeatures.at(i).x;
+          //       prevPts.at<double>(1,i) = prevFeatures.at(i).y;
+
+          //       currPts.at<double>(0,i) = currFeatures.at(i).x;
+          //       currPts.at<double>(1,i) = currFeatures.at(i).y;
+          //   }
+
+          //   //TODO: scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
+
+
+          //   if ((scale>0.1)&&(t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
+          //       //construct trajectory here:
+
+          //       t_f = t_f + scale*(R_f*t); //translation
+          //       R_f = R*R_f; //rotation
+
+          //   }
+
+          //   // try and find more keypoints if enough were found.
+          //   if (prevFeatures.size() < MIN_NUM_FEAT) {
+          //       std::cout << "Number of tracked features reduced to " << prevFeatures.size() << std::endl;
+          //       std::cout << "trigerring redection" << std::endl;
+          //       featureDetection(prevImage, prevFeatures);
+          //       featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
+
+          //   }
+
+          //   prevImage = currImage.clone();
+          //   prevFeatures = currFeatures;
+
+          //   sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm", t_f.at<double>(0), t_f.at<double>(1), t_f.at<double>(2));
+
+          //   std::cout << text << std::endl;
+    }
+    cv::waitKey(30);
+
+}
+catch (cv_bridge::Exception& e)
+{
+  ROS_ERROR("Could not convert from '%s' to 'bgr8'.", image->encoding.c_str());
+}
 }
 
 int main(int argc, char **argv)
